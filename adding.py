da0494fbf7188d82c5622f5517e7d9f655c93614
BushyWillie <104909104+BushyWillie@users.noreply.github.com>
2025-02-22 19:01:28 +0900

    added a new file and erased an old one

---
+++ adding.py
@@ -0,0 +1,18 @@
+def getAge():
+    birth_year = int(input("Please enter your birth year: "))
+    boolean = input("Have you had your birthday yet? (Yes or No)\n")
+    if boolean == "Yes":
+        year = 2025
+    else:
+        year = 2024
+    age = year - birth_year
+    return age
+
+def getPet():
+    pet = input("What is your pet's name? ")
+    return pet
+
+def getHobby():
+    hobby = input("What is your hobby, something to cause an error? ")
+    number = 1+1
+    return hobby
