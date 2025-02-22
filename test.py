556287fefb65dcd6271724772ad99db1ca5026f2
BushyWillie <104909104+BushyWillie@users.noreply.github.com>
2025-02-22 17:34:09 +0900

    Testing
    Testing the description

---
+++ test.py
@@ -0,0 +1,8 @@
+birth_year = int(input("Please enter your birth year: "))
+boolean = input("Have you had your birthday yet? (Yes or No)\n")
+if boolean == "Yes":
+    year = 2025
+else:
+    year = 2024
+age = year - birth_year
+print(f"Your age is {age}")
