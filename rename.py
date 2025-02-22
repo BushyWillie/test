45275aa4d7183b2679c5026b1acca5343700f14d
BushyWillie <104909104+BushyWillie@users.noreply.github.com>
2025-02-22 19:07:54 +0900

    changes

--- rename.py
+++ rename.py
@@ -13,6 +13,6 @@ def getPet():
     return pet
 
 def getHobby():
-    hobby = input("What is your hobby, something to cause an error? ")
+    hobby = input("What is your hobby? ")
     number = 1+1
     return hobby
